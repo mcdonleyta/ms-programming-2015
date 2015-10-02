9247f14d4ac53beb1d49cb239bfa940129e9f7f4
= <=>
2015-10-02 10:13:00 -0400

    Recommit guessing game

---
+++ guess.py
@@ -0,0 +1,19 @@
+# comment
+# R: Rchel Qu
+import random 
+
+guess  = 1 #This is user input
+secret = random.randint(0,100) ## Random number
+
+while guess != secret:
+    guess = raw_input("please enter a number")
+    guess = int(guess)
+    
+    
+    if guess < secret:
+		print "too low, Try again!"
+	elif guess > secret
+		print "too high,Try again!"
+	else:
+		print "You win"		
+		
