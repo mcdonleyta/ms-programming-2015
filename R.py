b49c4298e652b58713ac4c0ffa0214cc8d180d1f
= <=>
2015-10-07 09:57:35 -0400

    Turn in CYOA

--- R.py
+++ R.py
@@ -1,93 +1,24 @@
-inRoom = "main" 
-hasKey = 0 
-hasEscaped = 0 
- 
-while hasEscaped == 0: 
-if inRoom == "west": 
-print "You travel to a dusty room." 
-print "There is a key on the floor." 
-  
-print 
-print "1) Main Room" 
-print "2) Pick up Key" 
-print 
-  
-action = raw_input("choose #1-2: ") 
-action = int(action) 
-  
-if action == 1: 
-inRoom = "main" 
-if action == 2: 
-hasKey = 1 
- 
-if inRoom == "east": 
-print "You find a small bedroom long abandoned." 
-  
-print 
-print "1) Main Room" 
-print 
-  
-action = raw_input("choose #1: ") 
-action = int(action) 
-  
-if action == 1: 
-	inRoom = "main" 
- 
-if inRoom == "north": 
-print "It looks like this used to be a kitchen." 
-  
-print 
-print "1) Main Room" 
-print 
-  
-action = raw_input("choose #1: ") 
-action = int(action) 
-  
-if action == 1: 
-inRoom = "main" 
-  
-if inRoom == "south": 
-print "There is a locked door here." 
-  
-print 
-print "1) Main Room" 
-print "2) Unlock door" 
-print 
-  
-action = raw_input("choose #1-2: ") 
-action = int(action) 
-  
-if action == 1: 
-inRoom = "main" 
-if action == 2: 
-if hasKey == 1: 
-print "You have found the exit!" 
-print "You win!" 
-hasEscaped = 1 
-else: 
-print "You need a key for that!" 
-  
-if inRoom == "main": 
-print "You are in a scary warehouse." 
-print "Try to escape if you can!" 
-  
-print 
-print "1) Move North" 
-print "2) Move South" 
-print "3) Move West" 
-print "4) Move East" 
-print 
-  
-action = raw_input("choose #1-4: ") 
-action = int(action) 
-  
-if action == 1: 
-inRoom = "north" 
-if action == 2: 
-inRoom = "south" 
-if action == 3: 
-inRoom = "west" 
-if action == 4: 
-inRoom = "east" 
- 
-print "You won! Hurray \(^.^)/" 
+# comment
+# R: Rchel Qu
+import random 
+
+guess  = 1 #This is user input
+secret = random.randint(0,100) ## Random number
+
+while guess != secret:
+	guess = raw_input("please enter a number")
+	guess = int(guess)
+    
+    
+	if guess < secret:
+		print "too low, Try again!"
+	elif  guess > secret:
+		print "too high,Try again!"
+	else:
+		print "You win"	
+		
+		
+		
+
+
+
---
+++ helloworld.py
@@ -0,0 +1,24 @@
+# comment
+# R: Rachel
+import random 
+
+k = 100
+M = random.randint(0,100)
+
+
+k = raw_input("enter the number please")
+k = int(k)
+
+
+if k < M:
+   print "Too low man! Try again!"
+elif k > M:
+   print " What the high! Again"
+else:
+   print " Fun. You win!" 
+   
+   
+   
+   
+   
+   	
