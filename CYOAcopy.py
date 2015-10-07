c07daabf6671db32400262dfbc8721ab29037d4a
= <=>
2015-10-07 10:03:22 -0400

    Jacks CYOA!

---
+++ CYOAcopy.py
@@ -0,0 +1,120 @@
+user = raw_input("What is your name:")
+print "Hello" ,user
+print "Welcome"
+print "Your objective is to get out of the mansion alive if you can do it"
+print "Let's begin"
+
+inRoom = "main"
+hasKey = 0
+hasEscaped = 0
+
+while hasEscaped == 0:
+    if inRoom == "east":
+        print "You travel to a room with lots of saws."
+        print "There is a key on the floor, will you pick it up?"
+    
+        print
+        print "1) Main Room"
+        print "2) Pick up Key"
+        print
+        
+        action = raw_input("choose #1-2: ")
+        action = int(action)
+        
+        if action == 1:
+            inRoom = "main"
+        if action == 2:
+            hasKey = 1
+
+    if inRoom == "west":
+        print "You find a room with suspicious sounds."
+    
+        print
+        print "1) Main Room"
+        print "2) Kill Donkey"
+        
+        action = raw_input("choose #1-2: ")
+        action = int(action)
+        
+        if action == 1:
+            inRoom = "main"
+        if action == 2:
+			print "The donKEY takes your key from you"
+			hasKey = 0    
+        
+
+    if inRoom == "south":
+        print "It looks like this used to be an animal habitat."
+    
+        print
+        print "1) Main Room"
+        print
+        
+        action = raw_input("choose #1: ")
+        action = int(action)
+        
+        if action == 1:
+            inRoom = "main"
+    
+    if inRoom == "north":
+        print "There is a locked door here. It is a glass one too."
+    
+        print
+        print "1) Main Room"
+        print "2) Unlock door"
+        print
+        
+        action = raw_input("choose #1-2: ")
+        action = int(action)
+        
+        if action == 1:
+            inRoom = "main"
+        if action == 2:
+            if hasKey == 1:
+                print "You have found the exit!"
+                print "You win!"
+                hasEscaped = 1
+            else:
+                print "You need a key for that!"
+                
+    
+    if inRoom == "main":
+        print "You are in a scary warehouse."
+        print "Try to escape if you can!"
+          
+        print
+        print "1) Move North"
+        print "2) Move South"
+        print "3) Move West"
+        print "4) Move East"
+        print "5) Move Upstairs"
+        print 
+        
+        action = raw_input("choose #1-5: ")
+        action = int(action)
+        
+        if action == 1:
+            inRoom = "north"
+        if action == 2:
+            inRoom = "south"
+        if action == 3:
+            inRoom = "west"
+        if action == 4:
+            inRoom = "east"
+        if action == 5:
+			inRoom = "upstairs"
+			
+	if inRoom == "upstairs":
+		print "Theres nothing here! you wasted your time!"
+		
+		print
+		print "1) Main Room"
+		print 
+		
+		action = raw_input("chose #1-2: ")
+		action = int(action)
+		
+		if action == 1:
+			inRoom = "main"
+			
+
