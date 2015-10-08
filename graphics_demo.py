d3b3ab7b997020009b279705a9f322695976ef78
= <=>
2015-10-08 17:55:34 -0400

    turning in fixed choose ad Max Nanda

--- graphics_demo.py
+++ graphics_demo.py
@@ -1,7 +1,11 @@
 from easyGL import *
     
 def draw(): 
-  setColor(0, 0, 1)                          
-  drawRect(10, 10, 200, 100)
-    
-run(800, 600, "Mr McDonley's Awesome Example", draw)
+  setColor(1.4, 1.3, 1.5)                          
+  drawRect(100, 100, 200, 100)
+  setColor(1.4, 1.3, 1.5)                          
+  drawRect(214, 190, 780, 340)
+ 
+
+
+run(800,900, "MAX FACED UP DRAWING UP STUFF WITH MAX AND STUFF DRAWING STUFF UP MAX", draw)
