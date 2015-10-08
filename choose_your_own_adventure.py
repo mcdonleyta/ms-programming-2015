94f430e69d88f6b51bd731afe67de452dcbb24ab
= <=>
2015-10-08 09:38:07 -0400

    I changed something

--- choose_your_own_adventure.py
+++ choose_your_own_adventure.py
@@ -1,7 +1,7 @@
-inRoom = "main"
+inRoom = "main"#9278
 hasKey = 0 
 hasEscaped = 0 
- 
+
 while hasEscaped == 0: 
 	if inRoom == "west":
 		print
@@ -22,7 +22,7 @@ while hasEscaped == 0:
 			print
 			print '''The twin you asked answered,"The other twin would say that the left door is the correct one.'''
 			
-			print
+			print 
 			print "1) left door"
 			print "2) right door"
 			print
@@ -54,7 +54,7 @@ while hasEscaped == 0:
 				if action == 1: 
 					inRoom = "main" 
 				if action == 2: 
-					print "The note says, '9278'"
+					print "The note says, '92__'"
 					inRoom = "main" 
 		else: 
 			print
@@ -90,8 +90,9 @@ while hasEscaped == 0:
 				
 				if action2 == 1: 
 					inRoom = "main" 
-				if action2 == 2: 			
-					print "The note says, '9278'"
+				if action2 == 2: 
+					print			
+					print "The note says, '92__'"
 					inRoom = "main"
 					
 	if inRoom == "east":
@@ -110,12 +111,189 @@ while hasEscaped == 0:
 			inRoom = "main"
 		elif action == 2:
 			print
-			print "A person appeared."
-			print 'He says, "right, left, left, right"'
+			print "A troll appeared."
+			print 'He says, "I am a troll."'
+			print '"I will ask you five questions"'
+			print
+			print '"Question 1: What do bees use to make honey?"'
+			print "a) pollen"
+			print "b) flower petals"
+			print "c) nectar"
+			print "d) shoe soles"
+			print
 			
+			answer1 = raw_input("Type the letter of the correct answer:  ")
+			
+			if answer1 == "a":
+				print
+				print '"Wrong"'
+				print
+				print "The troll magically turned the room right-side up"
+				print "He made the ceiling vanish and you fell out of the world"
+				print
+				print "Game Over"
+				inRoom = "none"
+				
+			if answer1 == "b":
+				print
+				print '"Wrong"'
+				print
+				print "The troll magically turned the room right-side up"
+				print "He made the ceiling vanish and you fell out of the world"
+				print
+				print "Game Over"
+				inRoom = "none"
+				
+			if answer1 == "d":
+				print
+				print '"Wrong"'
+				print
+				print "The troll magically turned the room right-side up"
+				print "He made the ceiling vanish and you fell out of the world"
+				print
+				print "Game Over"
+				inRoom = "none"
+				
+			if answer1 == "c":
+				print
+				print '"Correct"'
+				print
+				
+				print '"Question 2: What is the name of the fifth planet from the sun?"'
+				print "a) Jupiter"
+				print "b) Mars"
+				print "c) Earth"
+				print "d) Saturn"
+				print
+				
+				answer2 = raw_input("Type the letter of the correct answer")
+					
+				if answer2 == "b":
+					print
+					print '"Wrong"'
+					print
+					print "The troll magically turned the room right-side up"
+					print "He made the ceiling vanish and you fell out of the world"
+					print
+					print "Game Over"
+					inRoom = "none"
+					
+				if answer2 == "d":
+					print
+					print '"Wrong"'
+					print
+					print "The troll magically turned the room right-side up"
+					print "He made the ceiling vanish and you fell out of the world"
+					print
+					print "Game Over"
+					inRoom = "none"
+					
+				if answer2 == "c":
+					print
+					print '"Wrong"'
+					print
+					print "The troll magically turned the room right-side up"
+					print "He made the ceiling vanish and you fell out of the world"
+					print
+					print "Game Over"
+					inRoom = "none"
+					
+				if answer2 == "a":
+					print
+					print '"Correct"'
+					print
+					print '"Question 3: What is the name of Cheetos\' mascot?"'
+					print "a) Cheesy"
+					print "b) Chester"
+					print "c) Cheeser"
+					print "d) Chessy"
+					print
+					
+					answer3 = raw_input("Type the letter of the correct answer")
+						
+					if answer3 == "d":
+						print
+						print '"Wrong"'
+						print
+						print "The troll magically turned the room right-side up"
+						print "He made the ceiling vanish and you fell out of the world"
+						print
+						print "Game Over"
+						inRoom = "none"
+						
+					if answer3 == "c":
+						print
+						print '"Wrong"'
+						print
+						print "The troll magically turned the room right-side up"
+						print "He made the ceiling vanish and you fell out of the world"
+						print
+						print "Game Over"
+						inRoom = "none"
+						
+					if answer3 == "a":
+						print
+						print '"Wrong"'
+						print
+						print "The troll magically turned the room right-side up"
+						print "He made the ceiling vanish and you fell out of the world"
+						print
+						print "Game Over"
+						inRoom = "none"
+						
+					if answer3 == "b":
+						print
+						print '"Correct"'
+						print
+						print '"Question 4: Poor people have it. Rich people need it. If you eat it you die."'
+						print
+					
+						answer4 = raw_input('"What is it?"')
+						
+						if answer4 != "Nothing":
+							print
+							print '"Wrong"'
+							print
+							print "The troll magically turned the room right-side up."
+							print "He made the ceiling vanish and you fell out of the world."
+							print
+							print "Game Over"
+							inRoom = "none"
+							
+						else:
+							print
+							print '"Correct"'
+							print
+							print '"Question 5: I occur twice in a week, once in a year but never in a day."'
+							print 
+							
+							answer5 = raw_input("What am I?")
+							
+							if answer5 != "The Letter E":
+								print
+								print '"Wrong"'
+								print
+								print "The troll magically turned the room right-side up."
+								print "He made the ceiling vanish and you fell out of the world."
+								print
+								print "Game Over"
+								inRoom = "none"
+							else:
+								print
+								print '"Correct"'
+								print
+								print '"You answered all the questions right!"'
+								print "The troll gave you a piece of paper"
+								print "It says, 'Right, Left, Left, Right'"
+								print
+								
+								inRoom = "main"
+								
+								
 	while inRoom == "north":
 	
 		if inRoom == "north": 
+			print
 			print "There are two ways you can go."
 			print "Which way do you go?"
 		  
@@ -135,7 +313,8 @@ while hasEscaped == 0:
 			elif action == 3:
 				inRoom = "main"
 				
-		if inRoom == "north": 
+		if inRoom == "north":
+			print 
 			print "There are two more ways you can go."
 			print "Which way do you go?"
 	  
@@ -156,6 +335,7 @@ while hasEscaped == 0:
 				inRoom = "main"
 			
 		if inRoom == "north": 
+			print
 			print "There are two more ways you can go."
 			print "Which way do you go?"
 		  
@@ -176,6 +356,7 @@ while hasEscaped == 0:
 				inRoom = "main"
 					
 		if inRoom == "north": 
+			print
 			print "There are two more ways you can go."
 			print "Which way do you go?"
 	  
@@ -197,6 +378,7 @@ while hasEscaped == 0:
 		
 		if inRoom == "north":		
 			if direcOne == "right" and direcTwo == "left" and direcThree == "left" and direcFour == "right":
+				print
 				print "You finished the maze. Congratulations!!!"
 				print "You find a key on the floor."
 				
@@ -215,16 +397,19 @@ while hasEscaped == 0:
 				if choose == 2:
 					inRoom = "main"
 		  
-	if inRoom == "south": 
+	if inRoom == "south":
+		print 
 		print "There is a locked door here." 
 		print "You need a password and a key to unlock the door"
+		print "There is another door to the right."
 		
 		print 
 		print "1) Main Room" 
 		print "2) Unlock door" 
+		print "3) Go to the door on the right."
 		print 
   
-		action = raw_input("choose #1-2: ") 
+		action = raw_input("choose #1-3: ") 
 		action = int(action) 
   
 		if action == 1: 
@@ -238,13 +423,46 @@ while hasEscaped == 0:
 					print 'You won! Hurray \(^.^)/'
 					hasEscaped = 1 
 				else:
+					print
 					print "That is the wrong password."
-			else: 
+					print
+			else:
+				print 
 				print "You need a key for that!" 
-  
-
-	
-	if inRoom == "main": 
+				print
+		if action == 3:
+			print
+			print "There is a bomb." 
+			fastPress = raw_input("Press the 7 key FAST to deactivate the bomb!")
+			fastPress = str(fastPress)
+			
+			if fastPress != "7":
+				print
+				print "You pressed the wrong key"
+				print 100 * "BOOM BAM! BOW!"
+				print "Game Over"
+				inRoom = "none"
+			else:
+				print 
+				print "You deactivated the bomb!"
+				print
+				print "You travel to a slimy room."
+				print "There is a note on the floor."
+				print
+				print "1) Pick up note"
+				print "2) Back to Main Room"
+				
+				action3 = raw_input("Choose #1-2")
+				action3 = int(action3)
+				
+				if action3 == 1:
+					print
+					print "The note says __78"
+				else:
+					inRoom = "main"
+					
+	if inRoom == "main":
+		print 
 		print "You are in a scary warehouse." 
 		print "Try to escape if you can!" 
   
@@ -267,3 +485,4 @@ while hasEscaped == 0:
 		if action == 4: 
 			inRoom = "east" 
 			
+	
---
+++ hello_you.py
@@ -0,0 +1,6 @@
+print "Hello"
+
+var_name = raw_input("What is your name?")
+str_name = str(var_name)
+
+print "Hello" + str_name
