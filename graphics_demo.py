48336dc893badd3b82a6df71523ed386bfe94196
= <=>
2015-10-22 09:46:36 -0400

    Checkin my drawing

--- graphics_demo.py
+++ graphics_demo.py
@@ -1,7 +1,31 @@
 from easyGL import *
-    
-def draw(): 
-  setColor(0, 0, 1)                          
-  drawRect(10, 10, 200, 100)
-    
-run(800, 600, "Mr McDonley's Awesome Example", draw)
+
+sunX = 500
+sunY = 500
+sunW = 100
+sunH = 100 
+sunMPF = 1   
+sunXY = 500
+def draw():
+	global sunXY, sunW, sunH, sunMPF 
+	setColor(0, 1, 1)
+	drawRect(0, 0, 800, 600) #Sky
+	
+	setColor(.38, 1, 0)
+	drawRect(0, 0, 800, 200)#Grass                         
+	   
+	setColor(0.90625, 0.87890625, 0.046875) #Sun
+	drawRect(sunXY, sunW, sunH)
+	
+	setColorA(1, 1, 1, .65) #Cloud
+	drawRect(300, 500, 100, 50)
+	
+	setColorA(1, 1, 1, .50) #Cloud
+	drawRect(325, 550, 50, 25)
+	     
+	drawRect(500,600,700,800)
+	
+	sunXY = sunXY - sunMPF    
+run(800, 600, "Kavins Kavin", draw)
+
+
