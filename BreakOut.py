b20887a1941e28244c858716ebc29944265a0124
= <=>
2015-11-13 10:05:22 -0500

    Alex's Brick Breaker Game Thing

--- BreakOut.py
+++ BreakOut.py
@@ -1,14 +1,32 @@
 from easyGL import *
+import random 
 
 screenWidth = 800
 screenHeight = 600
 numberBlocksPerRow = 8
-blockWidth = screenWidth / numberBlocksRow
+blockWidth = screenWidth / numberBlocksPerRow
 numberRows = 3
 blockHeight = (screenHeight/2)/2/numberRows
 
 def draw():
-	setColor(0, 0, 1)
-	drawRect(0, screenHeight-blockHeight, blockWidth, blockHeight)
+	
+	row = 0
+	blocky = 0
+	blocky = screenHeight-blockHeight
+	while row < numberRows:
+		block = 0
+		blockx = 0
+		while block < numberBlocksPerRow:
+			if row == 0:
+				setColor((row+block)/11.0, 0.3, 0.2)
+			if row ==1:
+				setColor((row+block)/14.0, 0.9, 0.1)
+			if row == 2:
+				setColor((row+block)/19.0, 0.6, 0.9)
+			drawRect(blockx, blocky, blockWidth, blockHeight)
+			block = block + 1
+			blockx = blockx + blockWidth
+		row = row + 1
+		blocky = blocky -blockHeight
 	
 run(screenWidth, screenHeight, "Breakout", draw)
---
+++ graphics_demo.py
@@ -0,0 +1,7 @@
+from easyGL import *
+    
+def draw(): 
+  setColor(0, 0, 1)                          
+  drawRect(10, 10, 200, 100)
+    
+run(800, 600, "Mr McDonley's Awesome Example", draw)
